741bd2d1d7a91c0c2e1f64aae548787f654be885
nnnnusui <nnnnusui@gmail.com>
2020-09-15 19:20:44 +0900

    :pencil2::new: command [save-all, save-to]

--- src/relocator.rs
+++ src/relocator.rs
@@ -1,10 +1,11 @@
 use std::thread::sleep;
 use std::time::Duration;
 
-use crate::position::Position;
 use crate::window::Window;
 use std::collections::HashMap;
+use std::fs::File;
 use std::mem;
+use std::path::Path;
 use std::sync::mpsc::{Receiver, Sender};
 use winapi::{
     shared::{
@@ -73,6 +74,8 @@ fn interpret_command(
         "show-all" => show_all(target_window),
         "state" => show_state(target_window, store),
         "save" => save(target_window, store),
+        "save-all" => save_all(target_window, store),
+        "save-to" => save_to(target_window, store, args[1]),
         _ => Err(target_window),
     }
 }
@@ -82,11 +85,7 @@ fn show(window: Window) -> Result<Window, Window> {
     Ok(window)
 }
 fn show_all(window: Window) -> Result<Window, Window> {
-    let windows = enumerate_windows();
-    let windows = windows
-        .iter()
-        .filter(|it| !is_target_of_reject(it))
-        .collect::<Vec<_>>();
+    let windows = get_windows();
     for window in &windows {
         println!("{:?}", window)
     }
@@ -106,14 +105,55 @@ fn save(window: Window, store: &mut HashMap<HWND, Window>) -> Result<Window, Win
     store.insert(window.hwnd, window.clone());
     Ok(window)
 }
+fn save_all(window: Window, store: &mut HashMap<HWND, Window>) -> Result<Window, Window> {
+    let windows = get_windows();
+    for window in windows {
+        store.insert(window.hwnd, window);
+    }
+    Ok(window)
+}
+fn save_to(
+    window: Window,
+    store: &mut HashMap<HWND, Window>,
+    file_path: &str,
+) -> Result<Window, Window> {
+    let file_path = file_path.to_string() + ".csv";
+    let file_path = Path::new(&file_path);
+    if !file_path.exists() {
+        File::create(&file_path).expect("Failed to create csv");
+    }
+    let mut writer = csv::Writer::from_path(&file_path).expect("Failed to create csv writer");
+    writer
+        .write_record(&["title", "class_name", "x", "y", "width", "height"])
+        .expect("Failed to write csv record");
+    for window in store.values() {
+        writer
+            .write_record(&[
+                &window.title,
+                &window.class_name,
+                &window.position.x.to_string(),
+                &window.position.y.to_string(),
+                &window.position.width.to_string(),
+                &window.position.height.to_string(),
+            ])
+            .expect("Failed to write csv record");
+    }
+    writer.flush().expect("Failed to save csv");
+    Ok(window)
+}
 
+fn get_windows() -> Vec<Window> {
+    enumerate_windows()
+        .into_iter()
+        .filter(|it| !is_target_of_reject(it))
+        .collect::<Vec<_>>()
+}
 fn enumerate_windows() -> Vec<Window> {
     let mut windows = Vec::<Window>::new();
     let userdata = &mut windows as *mut _;
     unsafe { EnumWindows(Some(enumerate_windows_callback), userdata as LPARAM) };
     windows
 }
-
 unsafe extern "system" fn enumerate_windows_callback(hwnd: HWND, userdata: LPARAM) -> BOOL {
     let windows: &mut Vec<Window> = mem::transmute(userdata);
     windows.push(Window::from(hwnd));
