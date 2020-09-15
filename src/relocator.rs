d0aa77f88f5b816f62bc7a0d61f36c486bb0d595
nnnnusui <nnnnusui@gmail.com>
2020-09-15 21:17:52 +0900

    :pencil2::new: :books:regex and command load

--- src/relocator.rs
+++ src/relocator.rs
@@ -2,12 +2,17 @@ use std::thread::sleep;
 use std::time::Duration;
 use thiserror::Error;
 
+use crate::position::Position;
 use crate::window::Window;
+use regex::Regex;
+use serde::ser::SerializeStruct;
+use serde::{Serialize, Serializer};
 use std::collections::HashMap;
 use std::fs::File;
 use std::mem;
 use std::path::Path;
 use std::sync::mpsc::{Receiver, Sender};
+use thiserror::private::PathAsDisplay;
 use winapi::{
     shared::{
         minwindef::{BOOL, LPARAM, TRUE},
@@ -38,10 +43,10 @@ pub fn standby_loop(receiver: &Receiver<String>) {
 
         let window = get_foreground_window();
         if window.hwnd != prev_window.hwnd && window.hwnd != default_window.hwnd {
-            if !is_target_of_reject(&window) {
-                println!("focus window: {:?}", window);
-                prev_window = window;
-            }
+            // if !is_target_of_reject(&window) {
+            println!("focus window: {:?}", window);
+            prev_window = window;
+            // }
         }
         match receiver.try_recv() {
             Ok(command) => {
@@ -77,6 +82,7 @@ fn interpret_command(
         "save" => save(target_window, store),
         "save-all" => save_all(target_window, store),
         "save-to" => save_to(target_window, store, args[1]),
+        "load" => load(target_window, store, args[1]),
         _ => Ok(target_window),
     }
 }
@@ -138,6 +144,39 @@ fn save_to(
     writer.flush()?;
     Ok(window)
 }
+fn load(
+    window: Window,
+    store: &mut HashMap<HWND, Window>,
+    file_path: &str,
+) -> Result<Window, Error> {
+    let file_path = file_path.to_string() + ".csv";
+    let file_path = Path::new(&file_path);
+    if !file_path.exists() {
+        panic!(format!("{} Not Found", file_path.as_display()))
+    }
+    let mut reader = csv::Reader::from_path(file_path)?;
+    for result in reader.deserialize() {
+        let record: Record = result?;
+        let title_regex = Regex::new(&record.title)?;
+        let class_name_regex = Regex::new(&record.class_name)?;
+        let windows = get_windows();
+        windows
+            .iter()
+            .filter(|window| {
+                title_regex.is_match(&window.title) && class_name_regex.is_match(&window.class_name)
+            })
+            .map(
+                |window| match window.clone().positioned_to(record.get_position()) {
+                    Ok(window) => window,
+                    Err(window) => window,
+                },
+            )
+            .for_each(|window| {
+                store.insert(window.hwnd, window);
+            });
+    }
+    Ok(window)
+}
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 struct Record {
@@ -148,6 +187,16 @@ struct Record {
     width: i32,
     height: i32,
 }
+impl Record {
+    fn get_position(&self) -> Position {
+        Position {
+            x: self.x,
+            y: self.y,
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
 
 #[derive(Error, Debug)]
 enum Error {
@@ -155,6 +204,8 @@ enum Error {
     CsvError(#[from] csv::Error),
     #[error("io error {0}")]
     IoError(#[from] std::io::Error),
+    #[error("regex error {0}")]
+    RegexError(#[from] regex::Error),
 }
 
 fn get_windows() -> Vec<Window> {
